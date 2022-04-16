4790a3547d4c462efc8468947c61920a90b10210
dkuklin <dkuklin@cindicator.com>
2022-04-16 16:53:28 +0300

    Fix tests

--- tests/test_stdbuf.py
+++ tests/test_stdbuf.py
@@ -1,103 +1,210 @@
 import asyncio
 import time
+from itertools import cycle
+from typing import Callable, Coroutine
 
 import pytest
 
 from stdbuf import Stdbuf
 
+PERF_TIME_DELTA = 1e-2
+
 
 @pytest.mark.asyncio
-async def test_simple():
+async def test_simple() -> None:
     with Stdbuf(3, 2.0, dedup=True) as buf:
         assert await buf.put(1)
         assert await buf.put(2)
         assert await buf.put(3)
-        assert not await buf.put(1)
         assert await buf.get() == [1, 2, 3]
         start = time.perf_counter()
         assert await buf.put(4)
-        assert not await buf.get_nowait()
+        assert buf.size() == 1
         assert await buf.get() == [4]
         elapsed = time.perf_counter() - start
-        assert (elapsed - 2.0) < 0.1
+        assert elapsed < 2.0 + PERF_TIME_DELTA
 
 
 @pytest.mark.asyncio
-async def test_producer_consumer():
-    BUF_SIZE = 5  # noqa
+async def test_ops() -> None:
+    with Stdbuf(2, 1.0, dedup=True) as buf:
+        assert buf.maxsize == 2
+        assert buf.maxtime - 1.0 < 1e-9
+        assert buf.empty()
+        assert await buf.put("a")
+        assert not buf.empty()
+        assert buf.size() == 1
+
+
+@pytest.mark.skip
+async def run(
+    maxsize: int,
+    maxtime: float,
+    dedup: bool,
+    consumer: Callable[[Stdbuf], Coroutine],
+    producer: Callable[[Stdbuf], Coroutine],
+    return_when: str = asyncio.FIRST_EXCEPTION,
+) -> None:
+    with Stdbuf(maxsize, maxtime, dedup) as buf:
+        done, pending = await asyncio.wait(
+            {
+                asyncio.create_task(producer(buf)),
+                asyncio.create_task(consumer(buf)),
+            },
+            return_when=return_when,
+        )
+        for task in pending:
+            task.cancel()
+        for task in done:
+            if task.exception():
+                raise task.exception()
+
+
+@pytest.mark.asyncio
+async def test_maxsize() -> None:
+    BUF_SIZE = 10  # noqa
+    BUF_TIME = 1.0  # noqa
+    N_ITEMS = 1000  # noqa
+
+    items = []
+
+    async def produce(buf: Stdbuf[int]):
+        for x in range(N_ITEMS):
+            await buf.put(x)
+        await buf.put(-1)
+        await asyncio.sleep(2 * BUF_TIME)
+
+    async def consume(buf: Stdbuf[int]):
+        while True:
+            start = time.perf_counter()
+            xs = await buf.get()
+            elapsed = time.perf_counter() - start
+
+            assert len(xs) <= BUF_SIZE
+            assert elapsed < BUF_TIME + PERF_TIME_DELTA
+
+            items.extend(xs)
+            if -1 in xs:
+                return
+
+    await run(BUF_SIZE, BUF_TIME, False, consume, produce)
+
+    assert sorted(items) == [-1] + list(range(N_ITEMS))
+
+
+@pytest.mark.asyncio
+async def test_maxtime() -> None:
+    BUF_SIZE = 10  # noqa
     BUF_TIME = 1.0  # noqa
 
-    async def produce(buf: Stdbuf[int]):
-        for x in range(20):
+    items = []
+
+    async def produce(buf: Stdbuf[float]):
+        for _ in range(BUF_SIZE):
+            x = time.perf_counter()
             await buf.put(x)
-            await asyncio.sleep(0.15)
-        for x in range(20, 40):
-            await buf.put(x)
-            await asyncio.sleep(0.3)
-
-    async def consume(buf: Stdbuf[int]):
-        while True:
-            start = time.perf_counter()
-            xs = await buf.get()
-            elapsed = time.perf_counter() - start
-            if not xs:
-                return
-            if xs[-1] < 20:
-                assert len(xs) == BUF_SIZE or elapsed < BUF_TIME + 1e-2
-            else:
-                assert len(xs) < BUF_SIZE and abs(elapsed - BUF_TIME) < 1e-2
-
-    with Stdbuf(BUF_SIZE, BUF_TIME, False) as buf:
-        done, pending = await asyncio.wait(
-            {
-                asyncio.create_task(consume(buf)),
-                asyncio.create_task(produce(buf)),
-            },
-            return_when=asyncio.FIRST_EXCEPTION,
-        )
-        for task in pending:
-            task.cancel()
-        for task in done:
-            if task.exception():
-                raise task.exception()
-
-
-@pytest.mark.asyncio
-async def test_producer_consumer_periodical():
-    BUF_SIZE = 10  # noqa
-    BUF_TIME = 3.0  # noqa
-
-    async def produce(buf: Stdbuf[int]):
-        for x in range(10):
-            await buf.put(x)
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(2 * BUF_TIME / BUF_SIZE)
         await asyncio.sleep(2 * BUF_TIME)
-        for x in range(10, 20):
-            await buf.put(x)
-            await asyncio.sleep(0.1)
+
+        await buf.put(time.perf_counter())
+        await buf.put(time.perf_counter())
         await asyncio.sleep(2 * BUF_TIME)
+
+        await buf.put(time.perf_counter())
+        await asyncio.sleep(2 * BUF_TIME)
+
         await buf.put(-1)
+        await asyncio.sleep(2 * BUF_TIME)
+
+    async def consume(buf: Stdbuf[float]):
+        while True:
+            xs = await buf.get()
+
+            assert len(xs) <= BUF_SIZE
+            assert max(xs) - min(xs) < BUF_TIME + PERF_TIME_DELTA
+
+            items.extend(xs)
+            if -1 in xs:
+                return
+
+    await run(BUF_SIZE, BUF_TIME, False, consume, produce)
+
+    assert len(items) == BUF_SIZE + 4
+
+
+@pytest.mark.asyncio
+async def test_dedup() -> None:
+    BUF_SIZE = 100  # noqa
+    BUF_TIME = 1.0  # noqa
+    N_ITEMS = 10000  # noqa
+
+    items = []
+
+    async def produce(buf: Stdbuf[str]):
+        success = 0
+        for char, _ in zip(cycle("Hello, world!"), range(N_ITEMS)):
+            if await buf.put(char):
+                success += 1
+
+        assert success == len(set("Hello, world!"))
+
+        await asyncio.sleep(2 * BUF_TIME)
+
+    async def consume(buf: Stdbuf[str]):
+        while True:
+            start = time.perf_counter()
+            chars = await buf.get()
+            elapsed = time.perf_counter() - start
+
+            assert elapsed < BUF_TIME + PERF_TIME_DELTA
+
+            items.extend(chars)
+
+    await run(BUF_SIZE, BUF_TIME, True, consume, produce, asyncio.FIRST_COMPLETED)
+
+    assert len(items) == len(set("Hello, world!"))
+
+
+@pytest.mark.asyncio
+async def test_multiple_producer() -> None:
+    BUF_SIZE = 100  # noqa
+    BUF_TIME = 1.0  # noqa
+
+    items = []
+
+    async def produce(buf: Stdbuf[int], begin: int, end: int):
+        for x in range(begin, end):
+            assert await buf.put(x)
+            await asyncio.sleep(BUF_TIME / 100)
+        await asyncio.sleep(2 * BUF_TIME)
 
     async def consume(buf: Stdbuf[int]):
         while True:
             start = time.perf_counter()
-            xs = await buf.get()
+            data = await buf.get()
             elapsed = time.perf_counter() - start
-            assert elapsed < BUF_TIME + 1e-2
-            assert len(xs) <= BUF_SIZE
-            if not xs:
-                # Empty buffer is OK.
-                continue
-            if xs[0] == -1:
-                return
 
-    with Stdbuf(BUF_SIZE, BUF_TIME, False) as buf:
+            assert len(data) <= BUF_SIZE
+            assert elapsed < BUF_TIME + PERF_TIME_DELTA
+
+            items.extend(data)
+
+    async def run():
+        await asyncio.gather(*producers)
+
+    with Stdbuf(BUF_SIZE, BUF_TIME, True) as buf:
+        producers = []
+        ranges = [(1000 * x, 1000 * y) for x, y in zip(range(0, 10), range(1, 11))]
+        for begin, end in ranges:
+            producer = asyncio.create_task(produce(buf, begin, end))
+            producers.append(producer)
+
         done, pending = await asyncio.wait(
             {
+                asyncio.create_task(run()),
                 asyncio.create_task(consume(buf)),
-                asyncio.create_task(produce(buf)),
             },
-            return_when=asyncio.FIRST_EXCEPTION,
+            return_when=asyncio.FIRST_COMPLETED,
         )
         for task in pending:
             task.cancel()
@@ -105,18 +212,4 @@ async def test_producer_consumer_periodical():
             if task.exception():
                 raise task.exception()
 
-
-@pytest.mark.asyncio
-async def test_explicit_stop():
-    buf = Stdbuf(2, 0.5, dedup=True)
-    assert await buf.put({"a": 1})
-    assert await buf.put({"b": 2})
-    assert not await buf.put({"c": 3})
-    assert await buf.get() == [{"a": 1}, {"b": 2}]
-    # Return buffer's content only if `maxsize` is reached.
-    buf.close()
-    assert await buf.put({"d": 4})
-    time.sleep(1)
-    assert not await buf.get_nowait()
-    assert await buf.put({"e": 5})
-    assert await buf.get() == [{"d": 4}, {"e": 5}]
+    assert sorted(items) == list(range(0, 10000))
